53b7c94f353fb8c29544d16216d914533158f69e
p-rahimi <parisa_rahimy@yahoo.com>
2025-05-21 20:11:29 +0300

    clean calculate function

--- app/Services/CurrencyService.php
+++ app/Services/CurrencyService.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Services;
+
 use App\Services\Contracts\CurrencyServiceInterface;
 
 class CurrencyService implements CurrencyServiceInterface
@@ -13,49 +14,40 @@ class CurrencyService implements CurrencyServiceInterface
         //
     }
 
-    public function calculate(array $rates, string $from, string $to, float $amount): float{
-        $rateFound = false;
-        $rateValue = 0;
-        $convertedAmount = 0;
-        foreach($rates as $rate) {
-            if ($rate['base_currency'] == $from && $rate['quote_currency'] == $to
-            ) {
-                $rateValue = $rate['quote'];
-                $rateFound = true;
-                break;
+    public function calculate(array $rates, string $from, string $to, float $amount): float
+    {
+        foreach ($rates as $rate) {
+            if ($rate['base_currency'] == $from && $rate['quote_currency'] == $to) {
+                return $this->roundResult($amount * $rate['quote']);
             } else if ($rate['base_currency'] == $to && $rate['quote_currency'] == $from) {
-                $rateValue = 1 / $rate['quote'];
-                $rateFound = true;
-                break;
+                return $this->roundResult($amount / $rate['quote']);
             }
         }
+
         $rateValueA = null;
         $rateValueB = null;
-
-        if($rateFound) {
-            $convertedAmount = $amount * $rateValue;
-        } else {
-            foreach($rates as $rate) {
-                if ($rate['base_currency'] == 'EUR' && $rate['quote_currency'] == $from
-                ) {
-                    $rateValueA = 1 / $rate['quote'];
-                } 
-                if ($rate['base_currency'] == 'EUR' && $rate['quote_currency'] == $to) {
-                    $rateValueB = $rate['quote'];
-                }
-                if ($rateValueA !== null && $rateValueB !== null) {
-                    break;
-                }
+        foreach ($rates as $rate) {
+            if ($rate['base_currency'] == 'EUR' && $rate['quote_currency'] == $from) {
+                $rateValueA = 1 / $rate['quote'];
             }
-
-            if ($rateValueA == null || $rateValueB == null) {
-                throw new \InvalidArgumentException('Conversion rate not found for the given currencies.');
+            if ($rate['base_currency'] == 'EUR' && $rate['quote_currency'] == $to) {
+                $rateValueB = $rate['quote'];
             }
-
-            $convertedAmount = $amount * $rateValueA * $rateValueB;
+            if ($rateValueA !== null && $rateValueB !== null) {
+                break;
+            }
         }
 
-        $convertedAmount = round($convertedAmount, 2);
-        return $convertedAmount;
+        if ($rateValueA == null || $rateValueB == null) {
+            throw new \InvalidArgumentException('Conversion rate not found for the given currencies.');
+        }
+
+        $convertedAmount = $amount * $rateValueA * $rateValueB;
+        return $this->roundResult($convertedAmount);
+    }
+
+    private function roundResult(float $input): float
+    {
+        return round($input, 2);
     }
 }
